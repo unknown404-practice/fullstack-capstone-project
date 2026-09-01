9cf09ff29a77e4404b8a1dedf1f578760f12a4ee
unknown404-practice <peter1280@tutamail.com>
2026-09-01 04:15:59 -0400

    Remove unused navigate variable to fix CI build warning

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
@@ -11,7 +11,6 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  const navigate = useNavigate();
 
   return (
     <>
